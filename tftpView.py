1501081b1b539f8d79431c6906aef46b92944a05
Carlos Montoya <cmontoya@TSOColombia.com>
2019-03-22 11:24:05 -0500

    Corrected Warnings from peer Review

--- tftpView.py
+++ tftpView.py
@@ -4,12 +4,12 @@ import tempfile
 import settings
 
 class MainWindow(QtWidgets.QMainWindow, Ui_Consultar_Scripts):
-    def __init__(self, *args, **kwargs):        
-        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)        
-        self.setupUi(self)        
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)
         self.label.setText("INGRESA LOS NUMEROS SERIALES")
         self.resultDict={}
-        self.errorMessage=QtWidgets.QErrorMessage()        
+        self.errorMessage=QtWidgets.QErrorMessage()
         self.label_2.setText("SCRIPTS ASIGNADOS")
         self.queryScripts.clicked.connect(self.findScript)   
 
@@ -24,16 +24,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Consultar_Scripts):
                     line=line.split()
                     self.resultDict[filename[0:12]]=line[len(line)-1]
         except Exception:
-            print()    
+            print()
 
     def findScript(self):
         tftp_server =settings.FTP_SERVER
         user=settings.FTP_USER
         password=settings.FTP_PWD        
-        try:             
+        try:
          ftp = ftplib.FTP(tftp_server,user,password)
         except OSError:
-            self.errorMessage.setWindowTitle("ERROR") 
+            self.errorMessage.setWindowTitle("ERROR")
             self.errorMessage.showMessage("SE REQUIERE CONEXION A LA VPN TSO")
             pass        
         else:
@@ -41,27 +41,26 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Consultar_Scripts):
             self.assignedScripts.setRowCount(len(serials))
             self.assignedScripts.setColumnCount(2)
             self.assignedScripts.setHorizontalHeaderLabels(("SERIAL", "SCRIPT ASIGNADO"))
-            tmpdir=tempfile.TemporaryDirectory() 
+            tmpdir=tempfile.TemporaryDirectory()
             self.assignedScripts.setColumnWidth(1,510)
-            for serial in serials:            
+            for serial in serials:
                 if (serial.isnumeric() and len(serial) == 12 ):
                     serial=serial+".cfg"
-                    self.getFile(ftp,serial,tmpdir.name)   
+                    self.getFile(ftp,serial,tmpdir.name)
                 else:
-                    self.resultDict[serial]="NO ES UN SERIAL VALIDO"                                         
+                    self.resultDict[serial]="NO ES UN SERIAL VALIDO"
             i=0
             for key,value in self.resultDict.items():
-                self.assignedScripts.setItem(i,0, QtWidgets.QTableWidgetItem(key))            
+                self.assignedScripts.setItem(i,0, QtWidgets.QTableWidgetItem(key))
                 self.assignedScripts.setItem(i,1, QtWidgets.QTableWidgetItem(value))
                 i=i+1
-            self.statusBar.showMessage("LISTO", 2000)         
+            self.statusBar.showMessage("LISTO", 2000)
             ftp.close()
             del tmpdir
-            self.resultDict={}
-            pass
+            self.resultDict={}            
         finally:
             pass
-            
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = MainWindow()
